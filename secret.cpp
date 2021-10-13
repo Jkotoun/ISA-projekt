54e9fef977d4bbdf0012ca86cb91548cfa9e8cfb
Jkotoun <jkotoun58@gmail.com>
2021-10-13 16:26:52 +0200

    reading file, icmp packet structure wip

--- secret.cpp
+++ secret.cpp
@@ -1,43 +1,83 @@
 #include <iostream>
 #include <getopt.h>
 #include <string>
-
-int main(int argc, char *argv[]) {
+#include <fstream>
+#include <vector>
+#include <pcap.h> 
+#include <netinet/ip_icmp.h> 
+using namespace std;
+int main(int argc, char *argv[])
+{
     //arguments parsing using getopt (-r <file> -s <ip|hostname> [-l])
     int current_arg;
-    std::string file = "";
-    std::string host = "";
-    bool listen_mode = false; 
-    while((current_arg = getopt(argc, argv, "r:s:l")) != -1)
+    string file_arg = "";
+    string host = "";
+    bool listen_mode = false;
+    while ((current_arg = getopt(argc, argv, "r:s:l")) != -1)
     {
-        switch(current_arg)
+        switch (current_arg)
         {
-            case 'r':
-            {
-                file = optarg;
-                continue;
-            }
-            case 's':
-            {
-                host = optarg;
-                continue;
-            }
-            case 'l':
-            {
-                listen_mode = true;
-                break;
-            }
-            case -1:
+        case 'r':
+        {
+            file_arg = optarg;
+            continue;
+        }
+        case 's':
+        {
+            host = optarg;
+            continue;
+        }
+        case 'l':
+        {
+            listen_mode = true;
+            break;
+        }
+        case -1:
             break;
         }
     }
-    if(file == "" || host == "")
+    if (file_arg == "" || host == "")
     {
-        std::cerr<<"-s and -r options are required"<<std::endl;
+        cerr << "-s and -r options are required" << endl;
         exit(1);
     }
+    if (listen_mode) //server
+    {
+    }
+    else //client
+    {
+        int packet_number = 0;
+        string a;
+        ifstream source_file(file_arg, ios::in);
+        vector<char> buffer(1000);
+        while (!source_file.eof())
+        {
+            source_file.read(buffer.data(), buffer.size());
+            streamsize s = source_file.gcount();
+            packet_number++;
+        }
 
-    if ()
+        source_file.close();
+
+
+
+
+
+    //ICMP header
+    struct icmp icmp_header;
+    icmp_header.icmp_type = ICMP_ECHO;
+    icmp_header.icmp_code = 0;
+    icmp_header.icmp_id = htons(99);
+    icmp_header.icmp_seq = htons(packet_number);
+    
+
+
+    icmp_header.icmp_cksum = 0;
+
+
+
+
+    }
 
     return 0;
-    }
+}
